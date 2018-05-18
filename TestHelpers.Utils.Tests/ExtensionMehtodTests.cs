0476a28685d3a9e578ef41bbef5daa5792226078
Unknown <patrik.olausson@hotmail.se>
2018-05-18 08:57:41 +0200

    Added basic extension methods and some initial tests

---
+++ TestHelpers.Utils.Tests/ExtensionMehtodTests.cs
@@ -0,0 +1,48 @@
+﻿using TestHelpers.Utils;
+using Xunit;
+
+namespace ExtensionMehtodTests
+{
+    public class ReplaceAllMatches
+    {
+        [Fact]
+        public void GivenAValueThatHasOneOccuranceOfTheValueToReplace_ThenItReplacesTheValueAsExpected()
+        {
+            var value = "Some text";
+
+            var result = value.ReplaceAllMatches("text", "fun!");
+
+            Assert.Equal("Some fun!", result);
+        }
+
+        [Fact]
+        public void GivenAValueThatHasTwoOccurancesOfTheValueToReplace_ThenItReplacesBothValuesAsExpected()
+        {
+            var value = "Some text with a word and a word";
+
+            var result = value.ReplaceAllMatches("word", "loud WORD");
+
+            Assert.Equal("Some text with a loud WORD and a loud WORD", result);
+        }
+
+        [Fact]
+        public void GivenAValueThatHasNoOccurancesOfTheValueToReplace_ThenItRetrunsTheOriginalValue()
+        {
+            var value = "Some text that shouldn't change";
+
+            var result = value.ReplaceAllMatches("Unknown");
+
+            Assert.Equal("Some text that shouldn't change", result);
+        }
+
+        [Fact]
+        public void GivenAValueThatHasTwoMatchesUsingARegularExpression_ThenItReplacesBothValuesAsExpected()
+        {
+            var value = "Cat, Hat, Bat";
+
+            var result = value.ReplaceAllMatches("[CB]at", "Replaced");
+
+            Assert.Equal("Replaced, Hat, Replaced", result);
+        }
+    }
+}
---
+++ TestHelpers.Utils/ExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace TestHelpers.Utils
+{
+    public static class ExtensionMethods
+    {
+        public static string ReplaceAllMatches(
+            this string value,
+            string valueToReplace,
+            string replacementValue = "ReplacedValue")
+        {
+            return new Regex(valueToReplace).Replace(value, replacementValue);
+        }
+
+        public static bool LooksLikeItContainsJson(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return false;
+            if (value.StartsWith("{")) return true;
+            if (value.StartsWith("[")) return true;
+
+            return false;
+        }
+
+        public static string ToJsonString(this object objectToSerialize, bool indented = true)
+        {
+            return JsonConvert.SerializeObject(
+                objectToSerialize,
+                indented ? Formatting.Indented : Formatting.None);
+        }
+
+        public static T FromJsonString<T>(this string jsonContent)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonContent);
+        }
+    }
+}
