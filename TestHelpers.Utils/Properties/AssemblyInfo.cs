e39d94bec4e8f0c42a85387e9b008a55188849c2
Tritonalite <patrik.olausson@hotmail.se>
2018-05-19 06:06:15 +0200

    Added nuspec file and bumped version

--- TestHelpers.Utils/Properties/AssemblyInfo.cs
+++ TestHelpers.Utils/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestHelpers.Utils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fundamental helper methods that simplifies testing")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Patrik Olausson")]
 [assembly: AssemblyProduct("TestHelpers.Utils")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Patrik Olausson Copyright ©  2018")]
+[assembly: AssemblyTrademark("Tritonalite")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
